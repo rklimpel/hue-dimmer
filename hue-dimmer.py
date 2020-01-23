e7363a0d6eb266ff1cbc53208149346fe439a79b
Rico Klimpel <rico@tklimpel.de>
2020-01-23 23:31:04 +0100

    args

--- hue-dimmer.py
+++ hue-dimmer.py
@@ -3,41 +3,47 @@
 from phue import Bridge
 import json
 import time
+import sys
+
+
+dim_direction = sys.argv[1]
+dim_duration = sys.argv[2]
 
 b = Bridge('192.168.0.106')
-
 b.connect()
+lights = b.lights
 
 #with open('data.json', 'w') as fp:
 #	json.dump(b.get_api(), fp, sort_keys=True, indent=4)
 
-lights = b.lights
-
-dim_time = 60*30
+dim_time = 60*int(dim_duration)
 step_time = dim_time/(255/1)
-if step_time >= 30:
-	trans_time = 30
-	wait_time = step_time - trans_time
-else:
-	trans_time = step_time
-	wait_time = 0
 
 print("step_time: " + str(step_time))
-print("trans_time: " + str(trans_time))
 print("dim_time: " + str(dim_time))
+
+if dim_direction == "up":
+	value_from = 0
+	value_to = 255
+	value_inc = 1
+elif dim_direction == "down":
+	value_from = 255
+	value_to = 0
+	value_inc = -1
 
 for light in lights:
 	light.on = True
 
-for brightness in range(255,0,-1):
+for brightness in range(value_from,value_to,value_inc):
 	print("Set brightness to " + str(brightness))
 	for light in lights:
 		light.transitiontime = 3
 		light.brightness = brightness
-	time.sleep(wait_time)
+	time.sleep(step_time)
 
-for light in lights:
-	light.on = False
+if dim_direction == "down":
+	for light in lights:
+		light.on = False
 
 
 
