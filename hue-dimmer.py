71ba147c71ecfa746b83aaecc5b8c1a1e0241668
rklimpel <rico@tklimpel.de>
2020-01-23 22:54:49 +0100

    do stuff

--- hue-dimmer.py
+++ hue-dimmer.py
@@ -8,19 +8,19 @@ b = Bridge('192.168.0.106')
 
 b.connect()
 
-with open('data.json', 'w') as fp:
-	json.dump(b.get_api(), fp, sort_keys=True, indent=4)
+#with open('data.json', 'w') as fp:
+#	json.dump(b.get_api(), fp, sort_keys=True, indent=4)
 
 lights = b.lights
 
 dim_time = 60*1
-trans_time = dim_time/255-60
+trans_time = dim_time/(255/5)
 print(trans_time)
 
 for light in lights:
 	light.on = True
 
-for brightness in range(255,0,-1):
+for brightness in range(255,0,-5):
 	print("Set brightness to " + str(brightness))
 	for light in lights:
 		light.transitiontime = 0
