f2f76bdf63a3e89a8b39cd876fba1ac1622bd6e4
Rico Klimpel <rico@tklimpel.de>
2020-01-23 22:35:53 +0100

    Add files via upload

---
+++ hue-dimmer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from phue import Bridge
+import json
+import time
+
+b = Bridge('192.168.0.106')
+
+b.connect()
+
+with open('data.json', 'w') as fp:
+	json.dump(b.get_api(), fp, sort_keys=True, indent=4)
+
+lights = b.lights
+
+dim_time = 60*1
+trans_time = dim_time/255-60
+print(trans_time)
+
+for light in lights:
+	light.on = True
+
+for brightness in range(255,0,-1):
+	print("Set brightness to " + str(brightness))
+	for light in lights:
+		light.transitiontime = 0
+		light.brightness = brightness
+	time.sleep(trans_time)
+
+for light in lights:
+	light.on = False
+
+
+
