342956dd4f8652bb850ec05ce1a6652da7627ea6
Rico Klimpel <rico@tklimpel.de>
2020-01-23 23:02:54 +0100

    transition stuff

--- hue-dimmer.py
+++ hue-dimmer.py
@@ -14,8 +14,17 @@ b.connect()
 lights = b.lights
 
 dim_time = 60*1
-trans_time = dim_time/(255/5)
-print(trans_time)
+step_time = dim_time/(255/5)
+if step_time >= 30:
+	trans_time = 30
+	wait_time = step_time - trans_time
+else:
+	trans_time = step_time
+	wait_time = 0
+
+print("step_time: " + str(step_time))
+print("trans_time: " + str(trans_time))
+print("dim_time: " + str(dim_time))
 
 for light in lights:
 	light.on = True
@@ -23,9 +32,9 @@ for light in lights:
 for brightness in range(255,0,-5):
 	print("Set brightness to " + str(brightness))
 	for light in lights:
-		light.transitiontime = 0
+		light.transitiontime = trans_time
 		light.brightness = brightness
-	time.sleep(trans_time)
+	time.sleep(wait_time)
 
 for light in lights:
 	light.on = False
